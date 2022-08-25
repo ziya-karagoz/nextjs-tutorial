583a5ce0de57136c9343f189be98ee715cdbacdf
Ziya <sar581999@gmail.com>
2022-08-25 13:25:42 +0300

    routing

---
+++ pages/blog/first.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FirstBlog() {
+  return <div>FirstBlog</div>;
+}
+
+export default FirstBlog;
---
+++ pages/blog/second.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SecondBlog() {
+  return <div>SecondBlog</div>;
+}
+
+export default SecondBlog;
